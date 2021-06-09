8cd25fa3eea885cb4a18180dff003a780e929f1a
Graham Albachten <albachteng@gmail.com>
2021-06-09 16:14:33 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import AppRoot from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AppRoot />
+  </React.StrictMode>,
+  document.getElementById('root')
+);

6da5b96af70157d796a2f78ab7e739af96cc09b8
Graham Albachten <albachteng@gmail.com>
2021-12-11 10:34:42 -0600

    separate central character logic into its own hook, readability

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,70 +1,36 @@
-import { useState, useEffect, Suspense } from 'react';
+import { Suspense } from 'react';
 import '../assets/css/App.css';
-import { useQuery } from '@apollo/client';
 import Display from './Display';
 import AbilityScoresDisplay from './AbilityScoresDisplay';
 import FeatureDisplay from './FeaturesDisplay';
 import HeaderDisplay from './HeaderDisplay';
 import SkillProficienciesDisplay from './SkillProficienciesDisplay';
-import dice /*, { limitedRange, LimitedRange } */ from '../utilities/dice';
 import HitPoints from './HitPoints';
-import { CHARACTERCLASSQUERY } from '../queries';
-import {CharacterClass, Race, AbilityScore } from '../types';
 import ChoicesDisplay from './ChoicesDisplay';
 import Inventory from './Inventory';
-
-const classesIndexArray: CharacterClass[] = ['barbarian', 'bard', 'cleric', 'druid', 'fighter', 'monk', 'paladin', 'ranger', 'ranger', 'sorcerer', 'warlock', 'wizard',];
-const racesIndexArray: Race[] = ['dragonborn', 'dwarf', 'elf', 'gnome', 'half-elf', 'half-orc', 'halfling', 'human', 'tiefling',];
-const getRandomClass = () => classesIndexArray[Math.floor(Math.random() * classesIndexArray.length)];
-const getRandomRace = () => racesIndexArray[Math.floor(Math.random() * racesIndexArray.length)];
-
-const emptyStats: AbilityScore = {
-  STR: 0,
-  CON: 0,
-  DEX: 0,
-  INT: 0, 
-  WIS: 0, 
-  CHA: 0,
-};
-
-const getRandomStats = (): AbilityScore => {
-  let newStats: any = {};
-  for (let key in emptyStats) {
-    newStats = {...newStats, [key]: dice.rollDice(6, 4, 0, 1)}
-  };
-  return newStats;
-};
+import useCharacter from '../hooks/store';
 
 const App = () => {
 
-  const [characterClass, setCharacterClass] = useState(getRandomClass());
-  const [characterRace, setCharacterRace] = useState(getRandomRace());
-  const [characterLevel, setCharacterLevel] = useState(1);
-  const [characterStats, setCharacterStats] = useState(getRandomStats());
-  const [proficiencyBonus, setProficiencyBonus] = useState(2);
-
-  useEffect(() => {
-    setProficiencyBonus(Math.floor((7 + characterLevel) / 4));
-  }, [characterLevel]);
-   
-  // variable form: {"FilterFindOneClassInput": {"index": "warlock"}}
-  const {loading, error, data} = useQuery(CHARACTERCLASSQUERY, {
-    variables: {"FilterFindOneClassInput": {"index": characterClass}}
-  });
-
-  const rerollStats = () => {
-    setCharacterStats(getRandomStats());
-  };
-
-  const newCharacter = () => {
-    setCharacterClass(getRandomClass());
-    setCharacterRace(getRandomRace());
-    setCharacterLevel(1);
-  };
-
-  const levelUp = () => {
-    setCharacterLevel(prev => prev + 1);
-  };
+    const {
+        characterClass,
+        setCharacterClass,
+        characterRace,
+        setCharacterRace,
+        characterLevel,
+        setCharacterLevel,
+        characterStats,
+        setCharacterStats,
+        proficiencyBonus,
+        setProficiencyBonus,
+        loading, 
+        error, 
+        data,
+        newCharacter,
+        levelUp,
+        rerollStats,
+    } = useCharacter();
+    
   return loading ? 'Loading...' : (
     <div className="App">
       <a target="_" href='https://www.dnd5eapi.co/graphql'>GraphQL Playground</a>
---
+++ src/hooks/store.tsx
@@ -0,0 +1,83 @@
+import { useState, useEffect, useReducer } from 'react';
+import { useQuery } from '@apollo/client';
+import dice /*, { limitedRange, LimitedRange } */ from '../utilities/dice';
+import { CHARACTERCLASSQUERY } from '../queries';
+import {CharacterClass, Race, AbilityScore } from '../types';
+
+const classesIndexArray: CharacterClass[] = ['barbarian', 'bard', 'cleric', 'druid', 'fighter', 'monk', 'paladin', 'ranger', 'ranger', 'sorcerer', 'warlock', 'wizard',];
+const racesIndexArray: Race[] = ['dragonborn', 'dwarf', 'elf', 'gnome', 'half-elf', 'half-orc', 'halfling', 'human', 'tiefling',];
+const getRandomClass = () => classesIndexArray[Math.floor(Math.random() * classesIndexArray.length)];
+const getRandomRace = () => racesIndexArray[Math.floor(Math.random() * racesIndexArray.length)];
+
+const emptyStats: AbilityScore = {
+  STR: 0,
+  CON: 0,
+  DEX: 0,
+  INT: 0, 
+  WIS: 0, 
+  CHA: 0,
+};
+
+const getRandomStats = (): AbilityScore => {
+  let newStats: any = {};
+  for (let key in emptyStats) {
+    newStats = {...newStats, [key]: dice.rollDice(6, 4, 0, 1)}
+  };
+  return newStats;
+};
+
+const useCharacter = () => {
+
+  const [characterClass, setCharacterClass] = useState(getRandomClass());
+  const [characterRace, setCharacterRace] = useState(getRandomRace());
+  const [characterLevel, setCharacterLevel] = useState(1);
+  const [characterStats, setCharacterStats] = useState(getRandomStats());
+  const [proficiencyBonus, setProficiencyBonus] = useState(2);
+
+
+  useEffect(() => {
+    setProficiencyBonus(Math.floor((7 + characterLevel) / 4));
+  }, [characterLevel]);
+   
+  // variable form: {"FilterFindOneClassInput": {"index": "warlock"}}
+  const {loading, error, data} = useQuery(CHARACTERCLASSQUERY, {
+    variables: {"FilterFindOneClassInput": {"index": characterClass}}
+  });
+
+  const rerollStats = () => {
+    setCharacterStats(getRandomStats());
+  };
+
+  const newCharacter = () => {
+    setCharacterClass(getRandomClass());
+    setCharacterRace(getRandomRace());
+    setCharacterLevel(1);
+  };
+
+  const levelUp = () => {
+    setCharacterLevel(prev => prev + 1);
+  };
+
+
+    return {
+        characterClass, 
+        setCharacterClass,
+        characterRace,
+        setCharacterRace,
+        characterLevel,
+        setCharacterLevel,
+        characterStats,
+        setCharacterStats,
+        proficiencyBonus,
+        setProficiencyBonus,
+        loading,
+        error,
+        data,
+        newCharacter,
+        levelUp,
+        rerollStats,
+    };
+    
+}
+
+export default useCharacter;

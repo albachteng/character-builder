247fce7cbb3369501ef5d3530358183c5870f66e
Albachten <albachtg@LEGAL.REGN.NET>
2021-12-30 13:14:00 -0600

    remove _id non-nullable in BackgroundEquipment

--- src/queries/BackgroundEquipment.js
+++ src/queries/BackgroundEquipment.js
@@ -3,7 +3,6 @@ import { gql } from '@apollo/client';
 export const BackgroundEquipment = gql`query BackgroundEquipment ($filter: FilterFindOneBackgroundInput) {
     background (filter: $filter) {
       name
-      _id
       starting_equipment {
         __typename
         quantity

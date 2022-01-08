2ce421b01cbee6e904716e08fdaebb0f461ea8c5
Graham Albachten <albachteng@gmail.com>
2022-01-08 13:08:25 -0600

    remove unnecessary leftover code from InventoryDisplay

--- src/components/InventoryDisplay.tsx
+++ src/components/InventoryDisplay.tsx
@@ -1,8 +1,7 @@
-import QueryMap, { MappingFunc } from './QueryMap';
+import { MappingFunc } from './QueryMap';
 import QueryRender from './QueryRender';
 import QueryWrapper from './QueryWrapper';
 import { ClassEquipmentOptions, ClassStartingEquipment, BackgroundEquipment } from "../queries";
-import { useQuery } from '@apollo/client';
 import withUseOption from './withUseOption';
 
 
@@ -13,43 +12,15 @@ type Props = {
 
 const InventoryDisplay = ({characterClass, characterBackground}: Props) => {
     
-    const { loading, error, data } = useQuery(ClassEquipmentOptions, {
-        variables: {"filter": {"index": characterClass}}
-      });
-
-    const startingEquipment = useQuery(ClassStartingEquipment, {
-        variables: {"filter": { "index": characterClass}}
-    });
-
     const equipmentMap: MappingFunc<{[key: string]: any}> = (item, index) => item && <li key={`${item?.equipment?.name}${index}`}>{item?.equipment?.name}: {item?.quantity}</li>;
 
     const QueryRenderWithUseOption = withUseOption(QueryRender);
 
     return (
         <>
-            {(loading || startingEquipment.loading) && 'Loading...'}
-            {(error || startingEquipment.error) && 'Whoops! Something went wrong!'}
-            {(data && startingEquipment.data) && (
                 <div>
                     <h2>Inventory:</h2>
                     <ul>
-                        <QueryMap 
-                            query={ClassStartingEquipment} 
-                            variables={{"filter": {"index": characterClass}}}
-                            mappingFunc={equipmentMap}
-                            dataType={["class", 'starting_equipment']}/>    
-                        <QueryMap 
-                            query={ClassEquipmentOptions} 
-                            variables={{"filter": {"index": characterClass}}}
-                            mappingFunc={equipmentMap}
-                            dataType={["class", 'starting_equipment_options']}
-                            useOption={true}/>    
-                        <QueryMap 
-                            query={BackgroundEquipment} 
-                            variables={{"filter": {"index": characterBackground}}}
-                            mappingFunc={equipmentMap}
-                            dataType={["background", 'starting_equipment']}/>    
-                        <h3>New Class Equipment Options</h3>
                         <QueryWrapper query={ClassStartingEquipment} variables={{"filter": {"index": characterClass}}} dataType={["class", "starting_equipment"]}>
                             <QueryRender mappingFunc={equipmentMap} data={{}}></QueryRender>
                         </QueryWrapper>
@@ -61,7 +32,6 @@ const InventoryDisplay = ({characterClass, characterBackground}: Props) => {
                         </QueryWrapper>
                     </ul>
                 </div>
-            )}
         </>
     )
 };

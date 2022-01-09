d03c86af52c007021bde05653082bcba941ff037
Graham Albachten <albachteng@gmail.com>
2022-01-09 11:56:23 -0600

    reduce boilerplate and reuse query wrapper and render map in skillsdisplay

--- src/components/SkillsDisplay.tsx
+++ src/components/SkillsDisplay.tsx
@@ -3,10 +3,11 @@ import { AllSkills } from "../queries";
 import SkillProficiencies from './SkillProficiencies';
 import { useQuery } from "@apollo/client";
 import type { AbilityScore } from '../types/AbilityScore';
-import { CharacterClass, Race, /* Skill */} from "../types";
+import { CharacterClass, Race, Skill } from "../types";
 import useSkillProficiencies from "../hooks/useSkillProficiencies";
-// import QueryWrapper from './QueryWrapper';
-// import RenderMap, { MappingFunc } from './RenderMap';
+import QueryWrapper from './QueryWrapper';
+import RenderMap, { MappingFunc } from './RenderMap';
+import SkillProficiency from './SkillProficiency';
 
 type Props = {
     characterClass: CharacterClass, 
@@ -19,15 +20,35 @@ type Props = {
 const SkillsDisplay = ({characterClass, characterStats, proficiencyBonus, characterRace, characterBackground}: Props) => {
 
     const { proficiencies } = useSkillProficiencies(characterClass, characterRace, characterBackground);
-    const { loading, error, data } = useQuery(AllSkills);
+    // const { skills } = useSkillProficiencies(characterClass, characterRace, characterBackground);
+    // const { loading, error, data } = useQuery(AllSkills);
 
-    // const mappingFunc: MappingFunc<Skill> = (skill) => {
-    //     return (<li>{skill.name}</li>);
-    // }
+    const isProficient = (proficiencies: Skill[], skill: Skill): [boolean, string | undefined] => {
+        for (let i = 0; i < proficiencies.length; i += 1) {
+            if (proficiencies[i] && proficiencies[i].index.slice(6)  === skill.index) {
+                return [true, proficiencies[i].__typename];
+            }
+        }
+        return [false, 'undefined'];
+    }
+
+    const makeProficienciesArray: MappingFunc<Skill> = (skill, index) => {
+        const proficiency = isProficient(proficiencies, skill);
+        return (
+            <SkillProficiency 
+                skill={skill} 
+                stat={characterStats[skill?.ability_score?.name]} 
+                proficiencyBonus={proficiencyBonus} 
+                isProficient={proficiency[0]}
+                key={`${skill.name}${index}`}
+                proficiencyFrom={proficiency[1]}
+            />
+        );
+    };
 
     return (
         <>
-            { loading && 'Loading...'}
+            {/* { loading && 'Loading...'}
             { error && 'Whoops, something went wrong!'}
             { data && 
                 <SkillProficiencies
@@ -36,10 +57,14 @@ const SkillsDisplay = ({characterClass, characterStats, proficiencyBonus, charac
                     skillState={proficiencies}
                     allSkills={data.skills}
                 />
-            }
-            {/* <QueryWrapper query={AllSkills} variables={{}} dataType={['skills']}>
-                <QueryRender mappingFunc={mappingFunc} data={{}}></QueryRender>
-            </QueryWrapper> */}
+            } */}
+            <h3>Skills 2 son of Skills</h3>
+            <QueryWrapper query={AllSkills} variables={{}} dataType={['skills']}>
+                <RenderMap mappingFunc={makeProficienciesArray} data={{}}></RenderMap>
+            </QueryWrapper>
+
+            {/* <h3>Proficiencies: </h3> */}
+            {/* <RenderMap mappingFunc={mappingFunc} data={proficiencies}></RenderMap> */}
         </>
     );
 };

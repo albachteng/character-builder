36ebf87627e81e40f32f7c0738f8aa8e99d0e957
Graham Albachten <albachteng@gmail.com>
2022-01-09 12:01:26 -0600

    remove SkillProficiencies, prefer QueryWrapper and RenderMap

--- src/components/SkillsDisplay.tsx
+++ src/components/SkillsDisplay.tsx
@@ -1,7 +1,5 @@
 /* renders skill proficiencies display and handles query for class skill choices */ 
 import { AllSkills } from "../queries";
-import SkillProficiencies from './SkillProficiencies';
-import { useQuery } from "@apollo/client";
 import type { AbilityScore } from '../types/AbilityScore';
 import { CharacterClass, Race, Skill } from "../types";
 import useSkillProficiencies from "../hooks/useSkillProficiencies";
@@ -20,8 +18,6 @@ type Props = {
 const SkillsDisplay = ({characterClass, characterStats, proficiencyBonus, characterRace, characterBackground}: Props) => {
 
     const { proficiencies } = useSkillProficiencies(characterClass, characterRace, characterBackground);
-    // const { skills } = useSkillProficiencies(characterClass, characterRace, characterBackground);
-    // const { loading, error, data } = useQuery(AllSkills);
 
     const isProficient = (proficiencies: Skill[], skill: Skill): [boolean, string | undefined] => {
         for (let i = 0; i < proficiencies.length; i += 1) {
@@ -48,23 +44,9 @@ const SkillsDisplay = ({characterClass, characterStats, proficiencyBonus, charac
 
     return (
         <>
-            {/* { loading && 'Loading...'}
-            { error && 'Whoops, something went wrong!'}
-            { data && 
-                <SkillProficiencies
-                    characterStats={characterStats}
-                    proficiencyBonus={proficiencyBonus}
-                    skillState={proficiencies}
-                    allSkills={data.skills}
-                />
-            } */}
-            <h3>Skills 2 son of Skills</h3>
             <QueryWrapper query={AllSkills} variables={{}} dataType={['skills']}>
                 <RenderMap mappingFunc={makeProficienciesArray} data={{}}></RenderMap>
             </QueryWrapper>
-
-            {/* <h3>Proficiencies: </h3> */}
-            {/* <RenderMap mappingFunc={mappingFunc} data={proficiencies}></RenderMap> */}
         </>
     );
 };

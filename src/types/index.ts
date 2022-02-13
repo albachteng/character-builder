fa6a55f7589126b7f2631e002dc7638734c97f08
Graham Albachten <albachteng@gmail.com>
2022-02-13 17:34:26 -0600

    replace unused types 'Skill'

--- src/types/index.ts
+++ src/types/index.ts
@@ -13,7 +13,6 @@ export type { CoreSkills } from './CoreSkills';
 export type { JSONValue } from './JSONValue';
 export type { NamedIndex } from './NamedIndex';
 export type { Race } from './Race';
-export type { Skill } from './Skill';
 export type { SkillIndex } from './SkillIndex';
 export type { SkillName } from './SkillName';
 export type { Store } from './Store';

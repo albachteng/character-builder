63f0f784a79421a07ccdc314b6b8659a1b7c4306
Graham Albachten <albachteng@gmail.com>
2022-02-19 11:25:48 -0600

    add test to sortByOptions

--- src/utilities/__tests__/sortByOptions.test.ts
+++ src/utilities/__tests__/sortByOptions.test.ts
@@ -1,7 +1,14 @@
 import { sortByOptions } from "../sortByOptions";
 
 describe('----- findArray -----', () => {
-    test('test runs', () => {
-        expect(1).toEqual(1);
-    })
+    test('it defaults to the default sort', () => {
+        const list = [5, 1, 2, 3];
+        const sorted = list.sort(sortByOptions['default']);
+        expect(sorted).toEqual([1, 2, 3, 5]);
+    });
+    test('it can sort by level', () => {
+        const list = [{level: 2}, {level: 1}, {level: 3}];
+        const sorted = list.sort(sortByOptions['levelAsc']);
+        expect(sorted).toEqual([{level: 1}, {level: 2}, {level: 3}]);
+    });
 })

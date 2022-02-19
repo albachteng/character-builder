5ff8887ca25d76bd8b102c9bd149ac843e885e88
Graham Albachten <albachteng@gmail.com>
2022-02-19 11:22:41 -0600

    add test to findArray

--- src/utilities/__tests__/findArray.test.ts
+++ src/utilities/__tests__/findArray.test.ts
@@ -1,7 +1,18 @@
 import { findArray } from "../findArray";
 
 describe('----- findArray -----', () => {
-    test('test runs', () => {
-        expect(1).toEqual(1);
+    test('it returns the nested data at the specified key', () => {
+        const data = {
+            find: {
+                this: ['you found me!']
+            }
+        };
+        const arr = findArray(data, ['find', 'this']);
+        expect(arr).toEqual(data.find.this);
+    });
+    test('it returns the data object itself if the key is not found', () => {
+        const data = {};
+        const arr = findArray(data, ['cannot', 'find', 'this']);
+        expect(arr).toEqual(data);
     })
 })

ec73c411265c9ef55e363448c7982dc20a959139
Graham Albachten <albachteng@gmail.com>
2022-06-30 17:30:21 -0500

    add fragments for traits and features

--- src/components/FeaturesDisplay/__generated__/FeaturesDisplayFragment_class.graphql.ts
+++ src/components/FeaturesDisplay/__generated__/FeaturesDisplayFragment_class.graphql.ts
@@ -1,5 +1,5 @@
 /**
- * @generated SignedSource<<c642e0c009d37102afc2676b9fb8fd4c>>
+ * @generated SignedSource<<31cc6f172a2f83b55b770c2ffd4d2b56>>
  * @lightSyntaxTransform
  * @nogrep
  */
@@ -13,10 +13,6 @@ import { FragmentRefs } from "relay-runtime";
 export type FeaturesDisplayFragment_class$data = {
   readonly class_levels: ReadonlyArray<{
     readonly ability_score_bonuses: number | null;
-    readonly class: {
-      readonly index: string | null;
-      readonly name: string | null;
-    } | null;
     readonly features: ReadonlyArray<{
       readonly choice: {
         readonly choose: number | null;
@@ -67,74 +63,11 @@ export type FeaturesDisplayFragment_class$data = {
     readonly index: string | null;
     readonly level: number | null;
     readonly prof_bonus: number | null;
-    readonly spellcasting: {
-      readonly cantrips_known: number | null;
-      readonly spell_slots_level_1: number | null;
-      readonly spell_slots_level_2: number | null;
-      readonly spell_slots_level_3: number | null;
-      readonly spell_slots_level_4: number | null;
-      readonly spell_slots_level_5: number | null;
-      readonly spell_slots_level_6: number | null;
-      readonly spell_slots_level_7: number | null;
-      readonly spell_slots_level_8: number | null;
-      readonly spell_slots_level_9: number | null;
-      readonly spells_known: number | null;
-    } | null;
     readonly subclass: {
       readonly index: string | null;
       readonly name: string | null;
     } | null;
   }>;
-  readonly hit_die: number | null;
-  readonly index: string | null;
-  readonly name: string | null;
-  readonly proficiencies: ReadonlyArray<{
-    readonly index: string | null;
-    readonly name: string | null;
-    readonly type: string | null;
-    readonly url: string | null;
-  }>;
-  readonly proficiency_choices: ReadonlyArray<{
-    readonly choose: number | null;
-    readonly from: ReadonlyArray<{
-      readonly index: string | null;
-      readonly name: string | null;
-    } | null> | null;
-    readonly type: string | null;
-  } | null> | null;
-  readonly saving_throws: ReadonlyArray<{
-    readonly index: string | null;
-    readonly name: string | null;
-  } | null> | null;
-  readonly spellcasting: {
-    readonly info: ReadonlyArray<{
-      readonly desc: ReadonlyArray<string | null> | null;
-      readonly name: string | null;
-    } | null> | null;
-    readonly level: number | null;
-    readonly spellcasting_ability: {
-      readonly index: string | null;
-      readonly name: string | null;
-    } | null;
-  } | null;
-  readonly starting_equipment: ReadonlyArray<{
-    readonly equipment: {
-      readonly index: string | null;
-      readonly name: string | null;
-    } | null;
-    readonly quantity: number | null;
-  } | null> | null;
-  readonly starting_equipment_options: ReadonlyArray<{
-    readonly choose: number | null;
-    readonly from: ReadonlyArray<{
-      readonly quantity: number | null;
-    } | null> | null;
-    readonly type: string | null;
-  } | null> | null;
-  readonly subclasses: ReadonlyArray<{
-    readonly index: string | null;
-    readonly name: string | null;
-  } | null> | null;
   readonly " $fragmentType": "FeaturesDisplayFragment_class";
 };
 export type FeaturesDisplayFragment_class$key = {
@@ -159,67 +92,42 @@ v2 = {
   "alias": null,
   "args": null,
   "kind": "ScalarField",
-  "name": "index",
+  "name": "choose",
   "storageKey": null
 },
 v3 = {
   "alias": null,
   "args": null,
   "kind": "ScalarField",
-  "name": "name",
+  "name": "type",
   "storageKey": null
 },
-v4 = [
-  (v2/*: any*/),
-  (v3/*: any*/)
-],
+v4 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "index",
+  "storageKey": null
+},
 v5 = {
   "alias": null,
   "args": null,
   "kind": "ScalarField",
-  "name": "choose",
+  "name": "name",
   "storageKey": null
 },
 v6 = {
   "alias": null,
   "args": null,
   "kind": "ScalarField",
-  "name": "type",
+  "name": "url",
   "storageKey": null
 },
 v7 = {
   "alias": null,
   "args": null,
   "kind": "ScalarField",
-  "name": "hit_die",
-  "storageKey": null
-},
-v8 = {
-  "alias": null,
-  "args": null,
-  "kind": "ScalarField",
-  "name": "url",
-  "storageKey": null
-},
-v9 = {
-  "alias": null,
-  "args": null,
-  "kind": "ScalarField",
-  "name": "desc",
-  "storageKey": null
-},
-v10 = {
-  "alias": null,
-  "args": null,
-  "kind": "ScalarField",
   "name": "__typename",
-  "storageKey": null
-},
-v11 = {
-  "alias": null,
-  "args": null,
-  "kind": "ScalarField",
-  "name": "quantity",
   "storageKey": null
 };
 return {
@@ -258,16 +166,6 @@ return {
         },
         {
           "alias": null,
-          "args": null,
-          "concreteType": "LevelClass",
-          "kind": "LinkedField",
-          "name": "class",
-          "plural": false,
-          "selections": (v4/*: any*/),
-          "storageKey": null
-        },
-        {
-          "alias": null,
           "args": [
             (v0/*: any*/)
           ],
@@ -284,8 +182,8 @@ return {
               "name": "choice",
               "plural": false,
               "selections": [
-                (v5/*: any*/),
-                (v6/*: any*/)
+                (v2/*: any*/),
+                (v3/*: any*/)
               ],
               "storageKey": null
             },
@@ -297,14 +195,26 @@ return {
               "name": "class",
               "plural": false,
               "selections": [
-                (v7/*: any*/),
-                (v2/*: any*/),
-                (v3/*: any*/),
-                (v8/*: any*/)
+                {
+                  "alias": null,
+                  "args": null,
+                  "kind": "ScalarField",
+                  "name": "hit_die",
+                  "storageKey": null
+                },
+                (v4/*: any*/),
+                (v5/*: any*/),
+                (v6/*: any*/)
               ],
               "storageKey": null
             },
-            (v9/*: any*/),
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "desc",
+              "storageKey": null
+            },
             {
               "alias": null,
               "args": null,
@@ -313,15 +223,15 @@ return {
               "name": "parent",
               "plural": false,
               "selections": [
-                (v2/*: any*/),
-                (v3/*: any*/),
-                (v8/*: any*/)
+                (v4/*: any*/),
+                (v5/*: any*/),
+                (v6/*: any*/)
               ],
               "storageKey": null
             },
-            (v2/*: any*/),
+            (v4/*: any*/),
             (v1/*: any*/),
-            (v3/*: any*/),
+            (v5/*: any*/),
             {
               "alias": null,
               "args": null,
@@ -331,7 +241,7 @@ return {
               "plural": true,
               "selections": [
                 (v1/*: any*/),
-                (v6/*: any*/)
+                (v3/*: any*/)
               ],
               "storageKey": null
             },
@@ -351,7 +261,7 @@ return {
                   "name": "subfeature_options",
                   "plural": false,
                   "selections": [
-                    (v5/*: any*/),
+                    (v2/*: any*/),
                     {
                       "alias": null,
                       "args": null,
@@ -360,14 +270,14 @@ return {
                       "name": "from",
                       "plural": true,
                       "selections": [
-                        (v10/*: any*/),
-                        (v2/*: any*/),
-                        (v3/*: any*/),
-                        (v8/*: any*/)
+                        (v7/*: any*/),
+                        (v4/*: any*/),
+                        (v5/*: any*/),
+                        (v6/*: any*/)
                       ],
                       "storageKey": null
                     },
-                    (v6/*: any*/)
+                    (v3/*: any*/)
                   ],
                   "storageKey": null
                 },
@@ -379,7 +289,7 @@ return {
                   "name": "expertise_options",
                   "plural": false,
                   "selections": [
-                    (v5/*: any*/),
+                    (v2/*: any*/),
                     {
                       "alias": null,
                       "args": null,
@@ -388,24 +298,24 @@ return {
                       "name": "from",
                       "plural": true,
                       "selections": [
-                        (v3/*: any*/),
-                        (v2/*: any*/),
-                        (v10/*: any*/)
+                        (v5/*: any*/),
+                        (v4/*: any*/),
+                        (v7/*: any*/)
                       ],
                       "storageKey": null
                     },
-                    (v6/*: any*/)
+                    (v3/*: any*/)
                   ],
                   "storageKey": null
                 }
               ],
               "storageKey": null
             },
-            (v8/*: any*/)
+            (v6/*: any*/)
           ],
           "storageKey": "features(sort:\"LEVEL_ASC\")"
         },
-        (v2/*: any*/),
+        (v4/*: any*/),
         {
           "alias": null,
           "args": null,
@@ -416,245 +326,17 @@ return {
         {
           "alias": null,
           "args": null,
-          "concreteType": "LevelSpellcasting",
-          "kind": "LinkedField",
-          "name": "spellcasting",
-          "plural": false,
-          "selections": [
-            {
-              "alias": null,
-              "args": null,
-              "kind": "ScalarField",
-              "name": "cantrips_known",
-              "storageKey": null
-            },
-            {
-              "alias": null,
-              "args": null,
-              "kind": "ScalarField",
-              "name": "spell_slots_level_1",
-              "storageKey": null
-            },
-            {
-              "alias": null,
-              "args": null,
-              "kind": "ScalarField",
-              "name": "spell_slots_level_2",
-              "storageKey": null
-            },
-            {
-              "alias": null,
-              "args": null,
-              "kind": "ScalarField",
-              "name": "spell_slots_level_3",
-              "storageKey": null
-            },
-            {
-              "alias": null,
-              "args": null,
-              "kind": "ScalarField",
-              "name": "spell_slots_level_4",
-              "storageKey": null
-            },
-            {
-              "alias": null,
-              "args": null,
-              "kind": "ScalarField",
-              "name": "spell_slots_level_5",
-              "storageKey": null
-            },
-            {
-              "alias": null,
-              "args": null,
-              "kind": "ScalarField",
-              "name": "spell_slots_level_6",
-              "storageKey": null
-            },
-            {
-              "alias": null,
-              "args": null,
-              "kind": "ScalarField",
-              "name": "spell_slots_level_7",
-              "storageKey": null
-            },
-            {
-              "alias": null,
-              "args": null,
-              "kind": "ScalarField",
-              "name": "spell_slots_level_8",
-              "storageKey": null
-            },
-            {
-              "alias": null,
-              "args": null,
-              "kind": "ScalarField",
-              "name": "spell_slots_level_9",
-              "storageKey": null
-            },
-            {
-              "alias": null,
-              "args": null,
-              "kind": "ScalarField",
-              "name": "spells_known",
-              "storageKey": null
-            }
-          ],
-          "storageKey": null
-        },
-        {
-          "alias": null,
-          "args": null,
           "concreteType": "LevelSubclass",
           "kind": "LinkedField",
           "name": "subclass",
           "plural": false,
-          "selections": (v4/*: any*/),
+          "selections": [
+            (v4/*: any*/),
+            (v5/*: any*/)
+          ],
           "storageKey": null
         }
       ],
-      "storageKey": null
-    },
-    (v7/*: any*/),
-    (v2/*: any*/),
-    (v3/*: any*/),
-    {
-      "alias": null,
-      "args": null,
-      "concreteType": "Proficiency",
-      "kind": "LinkedField",
-      "name": "proficiencies",
-      "plural": true,
-      "selections": [
-        (v2/*: any*/),
-        (v3/*: any*/),
-        (v6/*: any*/),
-        (v8/*: any*/)
-      ],
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "concreteType": "ClassProficiency_choices",
-      "kind": "LinkedField",
-      "name": "proficiency_choices",
-      "plural": true,
-      "selections": [
-        (v5/*: any*/),
-        {
-          "alias": null,
-          "args": null,
-          "concreteType": "ClassProficiency_choicesFrom",
-          "kind": "LinkedField",
-          "name": "from",
-          "plural": true,
-          "selections": (v4/*: any*/),
-          "storageKey": null
-        },
-        (v6/*: any*/)
-      ],
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "concreteType": "ClassSaving_throws",
-      "kind": "LinkedField",
-      "name": "saving_throws",
-      "plural": true,
-      "selections": (v4/*: any*/),
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "concreteType": "ClassSpellcasting",
-      "kind": "LinkedField",
-      "name": "spellcasting",
-      "plural": false,
-      "selections": [
-        {
-          "alias": null,
-          "args": null,
-          "concreteType": "ClassSpellcastingInfo",
-          "kind": "LinkedField",
-          "name": "info",
-          "plural": true,
-          "selections": [
-            (v9/*: any*/),
-            (v3/*: any*/)
-          ],
-          "storageKey": null
-        },
-        (v1/*: any*/),
-        {
-          "alias": null,
-          "args": null,
-          "concreteType": "ClassSpellcastingSpellcasting_ability",
-          "kind": "LinkedField",
-          "name": "spellcasting_ability",
-          "plural": false,
-          "selections": (v4/*: any*/),
-          "storageKey": null
-        }
-      ],
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "concreteType": "ClassStarting_equipment",
-      "kind": "LinkedField",
-      "name": "starting_equipment",
-      "plural": true,
-      "selections": [
-        {
-          "alias": null,
-          "args": null,
-          "concreteType": "ClassStarting_equipmentEquipment",
-          "kind": "LinkedField",
-          "name": "equipment",
-          "plural": false,
-          "selections": (v4/*: any*/),
-          "storageKey": null
-        },
-        (v11/*: any*/)
-      ],
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "concreteType": "ClassStarting_equipment_options",
-      "kind": "LinkedField",
-      "name": "starting_equipment_options",
-      "plural": true,
-      "selections": [
-        (v5/*: any*/),
-        {
-          "alias": null,
-          "args": null,
-          "concreteType": "ClassStarting_equipment_optionsFrom",
-          "kind": "LinkedField",
-          "name": "from",
-          "plural": true,
-          "selections": [
-            (v11/*: any*/)
-          ],
-          "storageKey": null
-        },
-        (v6/*: any*/)
-      ],
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "concreteType": "ClassSubclasses",
-      "kind": "LinkedField",
-      "name": "subclasses",
-      "plural": true,
-      "selections": (v4/*: any*/),
       "storageKey": null
     }
   ],
@@ -663,6 +345,6 @@ return {
 };
 })();
 
-(node as any).hash = "193585ee6fb9c19da22253c3165b0c90";
+(node as any).hash = "cdc1ccb8b27dcc6535f09f2ce0bccc5a";
 
 export default node;
--- src/components/FeaturesDisplay/index.tsx
+++ src/components/FeaturesDisplay/index.tsx
@@ -6,7 +6,8 @@ import {
   Choice,
   Maybe,
   FeatureFeature_SpecificSubfeature_OptionsFrom,
-  TraitTrait_SpecificSubtrait_OptionsFrom
+  TraitTrait_SpecificSubtrait_OptionsFrom,
+  CharacterClass
 } from "../../types";
 import { MappingFunc } from '../../types';
 import CharacterContext from '../CharacterContext';
@@ -15,28 +16,33 @@ import Feature from './Feature';
 import { makeUniqueId } from '@apollo/client/utilities';
 import RenderMap from '../RenderMap';
 import useFeaturesFilters from './useFeaturesFilters'
+import useTraitsFilters from './useTraitsFilters';
 import type { FeaturesDisplayFragment_class$key } from './__generated__/FeaturesDisplayFragment_class.graphql'
+import type { FeaturesDisplayFragment_race$key } from './__generated__/FeaturesDisplayFragment_race.graphql'
 import { useFragment } from 'react-relay';
 import { graphql } from 'babel-plugin-relay/macro';
 
 type Props = {
   classRef: FeaturesDisplayFragment_class$key
+  raceRef: FeaturesDisplayFragment_race$key
+  characterClass: CharacterClass
+  characterRace: Race
   // classFeatures: FeatureType[]
   // racialFeatures: Trait[]
   // backgroundFeatures: BackgroundFeature
 }
 
-function FeaturesDisplay({classRef}: Props): JSX.Element {
+function FeaturesDisplay({classRef, raceRef, characterClass, characterRace}: Props): JSX.Element {
 
-  const data = useFragment(
+  const {class_levels} = useFragment(
     graphql`fragment FeaturesDisplayFragment_class on Class {
       class_levels (sort: LEVEL_ASC, limit: $level) {
         level
         ability_score_bonuses
-        class {
-          index
-          name
-        }
+        # class {
+        #   index
+        #   name
+        # }
         features (sort: LEVEL_ASC){
           choice {
             choose
@@ -87,122 +93,149 @@ function FeaturesDisplay({classRef}: Props): JSX.Element {
         }
         index
         prof_bonus
-        spellcasting {
-          cantrips_known
-          spell_slots_level_1
-          spell_slots_level_2
-          spell_slots_level_3
-          spell_slots_level_4
-          spell_slots_level_5
-          spell_slots_level_6
-          spell_slots_level_7
-          spell_slots_level_8
-          spell_slots_level_9
-          spells_known
-        }
         subclass {
           index
           name
         }
       }
-      hit_die
-      index
-      name
-      proficiencies {
-        index
-        name
-        # reference {
-        #   index
-        #   name
-        #   type
-        #   url
-        # }
-        type
-        url
-      }
-      proficiency_choices {
-        choose
-        from {
-          index
-          name
-        }
-        type
-      }
-      saving_throws {
-        index
-        name
-      }
-      spellcasting {
-        info {
-          desc
-          name
-        }
-        level
-        spellcasting_ability {
-          index
-          name
-        }
-      }
-      starting_equipment {
-        equipment {
-          index
-          name
-        }
-        quantity
-      }
-      starting_equipment_options {
-        choose
-        from {
-          quantity
-        }
-        type
-      }
-      subclasses {
-        index
-        name
-      }
     }`, classRef);
-  // const { featureSpecificOptions, featureSpecificSelections } = useFeaturesFilters(classFeatures, racialFeatures);
-  //
-  // const whiteList: Array<Maybe<string> | undefined> = [];
-  // for (let key in featureSpecificSelections) {
-  //   if (featureSpecificSelections.hasOwnProperty(key)) whiteList.push(...featureSpecificSelections?.[key] as string) // TODO not super eficient, we should probably get this some other way
-  // }
-  //
-  // function featuresFilter(features: Array<FeatureType | Trait>) {
-  //   return features.filter((feature) => {
-  //     // if it's in options but not the whitelist, it should be filtered
-  //     for (let key in featureSpecificSelections) {
-  //       if (featureSpecificOptions?.[key]?.includes(feature?.index) && !whiteList?.includes(feature?.index)) {
-  //         return false;
-  //       }
-  //     }
-  //       return true;
-  //     })
-  // }
-  //
-  //
-  // const featuresMap: MappingFunc<FeatureType | Trait | BackgroundFeature> = (feature, index) => {
-  //   return (
-  //     <Feature
-  //       // show={show}
-  //       key={makeUniqueId('feature')}
-  //       feature={feature}
-  //       // featureSpecificSelections={featureSpecificSelections}
-  //     />
-  //   );
-  // };
-  //
+
+  const {traits} = useFragment(
+    graphql`fragment FeaturesDisplayFragment_race on Race {
+      traits {
+        desc
+        index
+        name
+        proficiencies {
+          index
+          name
+          type
+        }
+        proficiency_choices {
+          choose
+          from {
+            name
+            index
+            __typename
+          }
+          type
+        }
+        parent {
+          index
+          name
+        }
+        trait_specific {
+          subtrait_options {
+            choose
+            from {
+              __typename
+              index
+              name
+              url
+            }
+            type
+          }
+          spell_options {
+            choose
+            from {
+              name
+              index
+              __typename
+            }
+            type
+          }
+          damage_type {
+            index
+            name
+          }
+          breath_weapon {
+            name
+            desc
+            usage {
+              type
+              times
+            }
+            dc {
+              dc_type {
+                index
+                name
+              }
+              success_type
+            }
+            damage {
+              damage_at_character_level
+            }
+          }
+        }
+      }
+  }`, raceRef)
+
+  const classFeatures = class_levels.map((level) => level.features).flat();
+
+  const { featureSpecificOptions, featureSpecificSelections } = useFeaturesFilters(classFeatures, characterClass);
+  const { traitSpecificOptions, traitSpecificSelections } = useTraitsFilters(traits, characterRace)
+
+  const whiteList: Array<any> = [];
+  for (let key in featureSpecificSelections) {
+    if (featureSpecificSelections.hasOwnProperty(key)) whiteList.push(...featureSpecificSelections?.[key])
+    // TODO not super eficient, we should probably get this some other way
+  }
+  for (let key in traitSpecificSelections) {
+    if (traitSpecificSelections.hasOwnProperty(key)) whiteList.push(...traitSpecificSelections?.[key])
+    // TODO not super eficient, we should probably get this some other way
+  }
+
+  console.log({featureSpecificOptions, traitSpecificOptions, whiteList})
+  function featuresFilter(features: typeof classFeatures) {
+    return features.filter((feature) => {
+      // if it's in options but not the whitelist, it should be filtered
+      for (let key in featureSpecificSelections) {
+        if (featureSpecificOptions?.[key]?.includes(feature?.index) && !whiteList?.includes(feature?.index)) {
+          return false;
+        }
+      }
+        return true;
+      })
+  }
+
+  function traitFilter(traits: typeof traits) {
+    return traits.filter((trait) => {
+      // if it's in options but not the whitelist, it should be filtered
+      for (let key in traitSpecificSelections) {
+        if (featureSpecificOptions?.[key]?.includes(trait?.index) && !whiteList?.includes(trait?.index)) {
+          return false;
+        }
+      }
+        return true;
+      })
+  }
+
+
+  const featuresMap: MappingFunc<any> = (feature, index) => {
+    return (
+      <Feature
+        // show={show}
+        key={makeUniqueId('feature')}
+        feature={feature}
+        // featureSpecificSelections={featureSpecificSelections}
+      />
+    );
+  };
+
   return (
     <div style={{ height: '50%', overflow: 'scroll' }}>
       {/* <pre>{JSON.stringify(featureSpecificSelections, null, 2)}</pre> */}
       {/* <pre>{JSON.stringify(featureSpecificOptions, null, 2)}</pre> */}
       <h2>Features</h2>
-      <pre>{JSON.stringify(data?.class_levels, null, 2)}</pre>
-      {/* <RenderMap */}
-      {/*   mappingFunc={featuresMap} */}
-      {/*   data={[backgroundFeatures, ...featuresFilter([...racialFeatures, ...classFeatures])]} */}
-      {/* /> */}
+      {/* <pre>{JSON.stringify(class_levels, null, 2)}</pre> */}
+      <RenderMap
+        mappingFunc={featuresMap}
+        data={featuresFilter(classFeatures)}
+      />
+      <RenderMap
+        mappingFunc={featuresMap}
+        data={traitFilter(traits)}
+      />
     </div>
   );
 }
